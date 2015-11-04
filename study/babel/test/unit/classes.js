242321d254d031ec055ce5316f7c501c639310a0
Ainsley Chong <ainsley.chong@gmail.com>
2015-11-04 08:24:45 -0800

    Added classes

---
+++ study/babel/test/unit/classes.js
@@ -0,0 +1,40 @@
+
+describe('Classes', () => {
+  class Base {
+    constructor() {
+      this.baseVar = 1;
+    }
+  }
+
+  // Need to manually call super()?
+  class Sub1 extends Base {
+    constructor() {
+      super();
+      this.baseVar = 2;
+    }
+  }
+
+  class Sub2 extends Base {
+    constructor() {
+      super();
+      this.subVar = 3;
+    }
+  }
+
+  it('should correctly instantiate new instances', () => {
+    var base = new Base();
+    expect(base.baseVar).to.equal(1);
+  });
+
+  it('should correctly overwrite base variables', () => {
+    var sub1 = new Sub1();
+    expect(sub1.baseVar).to.equal(2);
+  });
+
+  it('should run subclass constructor with "super()"', () => {
+    var sub2 = new Sub2();
+    expect(sub2.baseVar).to.equal(1);
+    expect(sub2.subVar).to.equal(3);
+  });
+
+});
