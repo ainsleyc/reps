6e59ce591ee4aadc9cd1d0cd2c331a3f5dd69303
Ainsley Chong <ainsley.chong@gmail.com>
2015-11-06 09:06:51 -0800

    jscs

--- study/babel/test/unit/letConst.js
+++ study/babel/test/unit/letConst.js
@@ -1,8 +1,8 @@
 
 describe('Let, Const', () => {
   it('should create block scoping with let', () => {
-    let x = 1
-    if(true) {
+    let x = 1;
+    if (true) {
       let x = 2;
     }
     expect(x).to.equal(1);
