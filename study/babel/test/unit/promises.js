4a6af80191fe9486c12c75f0dd9d91db5045da46
Ainsley Chong <ainsley.chong@gmail.com>
2015-11-07 15:15:38 -0800

    Promises

--- study/babel/test/unit/promises.js
+++ study/babel/test/unit/promises.js
@@ -1,9 +1,27 @@
 
 describe('Promises', () => {
-  it('should', () => {
+  it('should allow promise syntax', () => {
+    var promise = new Promise(function(resolve, reject) {});
   });
 
-  it('should allow anonymous function declation', () => {
+  it('should trigger success clause', (done) => {
+    var promise = new Promise((resolve, reject) => {
+      resolve(1);
+    });
+    promise.then((result) => {
+      expect(result).to.equal(1);
+      done();
+    });
   });
 
+  it('should trigger failure clause', (done) => {
+    var promise = new Promise((resolve, reject) => {
+      reject(1);
+    });
+    promise.then((result) => {
+    }, (err) => {
+      expect(err).to.equal(1);
+      done();
+    });
+  });
 });
